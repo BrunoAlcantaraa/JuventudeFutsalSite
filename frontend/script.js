b16ccebe8435916aa33789bb792769f292ab3bc4
BrunoGraveto <alcantarasantosbruno@gmail.com>
2025-10-07 20:31:49 -0300

    feat(script.js): adiciona funcionalidade de receber info e popular os cards

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,6 @@
+// URL endpoint Java
+const API_URL = "http://localhost:8080/api/jogadores"
+
 /*
     Jogadores:
 */
@@ -97,6 +100,31 @@ const titulos = [
 ]
 
 /*
+    Função para buscar os jogadores no Banco:
+*/
+async function carregarJogadores(idDestino, idCard) {
+    try {
+        // Faz a requisição
+        const response = await fetch(API_URL);
+
+        // Verifica se está ok
+        if(!response.ok) {
+            throw new Error('Erro HTTP! Status ${response.status}');
+        }
+        // Obtem os dados
+        const dadosLista = await response.json();
+
+        // Esibe
+        exibir(dadosLista, idDestino, idCard)
+
+    } catch (error) {
+        console.error("Erro ao carregar dados do backend!", error)
+        const container = document.getElementById(idDestino);
+        container.innerHTML = `<p>Não foi possível carregar os dados. Tente novamente mais tarde.</p>`;
+    }
+}
+
+/*
     Função para adicionar Cards nos Containers:
 */
 function exibir(lista, idDestino, idCard) {
@@ -107,19 +135,30 @@ function exibir(lista, idDestino, idCard) {
         const card = document.createElement("div");
         card.classList.add(idCard);
 
-        card.innerHTML = `
-            <img src="${item.foto}" alt="${item.nome || item.colocacao}">
-            <h3>${item.nome || item.colocacao}</h3>
-            <p>${item.posicao || item.funcao || item.ano}</p>
-        `;
+        card.innerHTML = carregarHTMLCard(item);
 
         container.appendChild(card);
     });
 }
 
 /*
+    Função para retornar HTML ao devido caso:
+*/
+function carregarHTMLCard(item) {
+    if (item.fotoJogador) {
+        return `<img src="${item.fotoJogador}" alt="${item.nomeJogador}">
+            <h3>${item.nomeJogador}</h3>
+            <p>${item.posicaoJogador}</p>`;
+    } 
+    return `<img src="${item.foto}" alt="${item.nome || item.colocacao}">
+        <h3>${item.nome || item.colocacao}</h3>
+        <p>${item.posicao || item.funcao || item.ano}</p>
+    `;
+}
+
+/*
     Insere os Cards nos Containers:
 */
-exibir(jogadores, "lista-jogadores", "card");
-exibir(comissao, "lista-comissao", "card")
-exibir(titulos, "lista-titulos", "card-titulo")
+carregarJogadores("lista-jogadores", "card");// exibir(jogadores, "lista-jogadores", "card");
+exibir(comissao, "lista-comissao", "card");
+exibir(titulos, "lista-titulos", "card-titulo");
